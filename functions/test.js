1970af2c846187047798647ffa2ae98d98536bac
Singee <imsingee@gmail.com>
2019-07-20 15:51:21 +0800

    edit test function

--- functions/test.js
+++ functions/test.js
@@ -1,13 +1,16 @@
 exports.handler = function (event, context, callback) {
     const { queryStringParameters } = event;
 
-    const src = queryStringParameters['src'];
-    
+    let result = '';
+    for (const key of queryStringParameters) {
+        result += `${key} = ${queryStringParameters[key]}\n`;
+    }
+
     return callback(null, {
         headers: {
             "Content-Type": "text/plain; charset=utf-8"
         },
         statusCode: 200,
-        body: `成功。src=${src}`
+        body: `成功。\n${result}`
     });
 }
