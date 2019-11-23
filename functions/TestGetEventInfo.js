ff57b8d80d39365dc4c02c98b53d552202f12214
Singee <imsingee@gmail.com>
2019-11-23 10:15:55 +0800

    add(api/TestGetEventInfo): can get environment variables now

--- functions/TestGetEventInfo.js
+++ functions/TestGetEventInfo.js
@@ -1,9 +1,11 @@
 exports.handler = function (event, context, callback) {
+      const env = process.env;
+      
       return callback(null, {
             headers: {
                 "Content-Type": "text/plain; charset=utf-8"
             },
             statusCode: 200,
-            body: JSON.stringify({ event, context })
+            body: JSON.stringify({ event, context, env })
       });
 }
