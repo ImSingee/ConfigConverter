f4e61d97058546b415cbb03aa271856687e603fa
Singee <imsingee@gmail.com>
2019-11-23 09:45:53 +0800

    Add /api/QuantumultXScriptSubscriptionAddDeviceID

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 
 - `/api/SurgeProfile2SurgeList` 将 Surge 配置文件转换为 List
 - `/api/QuantumultXScriptAddDeviceID` 将 QX 脚本中添加设备 ID
+- `/api/QuantumultXScriptSubscriptionAddDeviceID` 将 QX rewrite 订阅中 script-response-body 类型的 url 修改为相应的 `/api/QuantumultXScriptAddDeviceID` 转换后的 url
---
+++ functions/QuantumultXScriptSubscriptionAddDeviceID.js
@@ -0,0 +1,79 @@
+const request = require('flyio');
+const isUrl = require('is-url');
+
+exports.handler = function (event, context, callback) {
+    const { queryStringParameters, headers } = event;
+    const url = queryStringParameters['src'];
+    const deviceId = queryStringParameters['id'];
+    const host = headers['Host'];
+    
+    console.log('url: ', url);
+    console.log('deviceId: ', deviceId);
+
+    if (!isUrl(url)) {
+        console.log('URL is invlid');
+        return callback(null, {
+            headers: {
+                "Content-Type": "text/plain; charset=utf-8"
+            },
+            statusCode: 400,
+            body: "参数 src 无效，请检查是否提供了正确的脚本文件托管地址。"
+        });
+    }
+    if (!deviceId) {
+        console.log('deviceId is not found');
+        return callback(null, {
+            headers: {
+                "Content-Type": "text/plain; charset=utf-8"
+            },
+            statusCode: 400,
+            body: "参数 id 无效，请检查是否提供了正确的设备 ID。"
+        });
+    }
+
+    request.get(url).then(({ data }) => {
+        console.log('File fetched success.');
+        const allLines = data.split('\n');
+        const resultLines = new Array();
+        
+        for (const singleLine of allLines) {
+            const singleLineTrimed = singleLine.trim();
+            if (singleLineTrimed == '') {
+                // Do nothing
+            } else if (singleLineTrimed.startsWith('hostname') {
+                resultLines.push(singleLineTrimed);
+            } else if (singleLineTrimed.startsWith('#') {
+                // Do nothing
+            } else if (singleLineTrimed.startsWith(';') {
+                // Do nothing
+            } else {
+                const currentLineElements = singleLineTrimed.split(/\s+/);
+                if (currentLineElements.length < 4) {
+                    resultLines.push(singleLineTrimed);
+                } else if (currentLineElements[2] != 'script-response-body') {
+                    resultLines.push(singleLineTrimed);
+                } else {
+                    currentLineElements[3] = `${host}/api/QuantumultXScriptSubscriptionAddDeviceID?id=${deviceId}&src=${url}`;
+                    resultLines.push(currentLineElements.join(' '));
+                }
+            }
+            
+        }
+
+        return callback(null, {
+            headers: {
+                "Content-Type": "text/plain; charset=utf-8"
+            },
+            statusCode: 200,
+            body: resultLines.join(' ')
+        });
+    }).catch(err => {
+        return callback(null, {
+            headers: {
+                "Content-Type": "text/plain; charset=utf-8"
+            },
+            statusCode: 400,
+            body: err
+        });
+    })
+}
