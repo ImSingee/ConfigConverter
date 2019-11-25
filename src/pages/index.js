f9a86977c02bdfea60b39a8058ee246f721aedc4
灰灰 <20509887+wangfei021325@users.noreply.github.com>
2019-11-25 09:01:26 +0800

    Update index.js
    / generator / QuantumultXScriptScriptionAddDeviceId

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ export default function() {
     <div className={styles.normal}>
         <div>The home page is developing.</div>
         <div>
-          <a href="https://github.com/ImSingee/ConfigConverter">
+          <a href="https://github.com/wangfei021325/ConfigConverter">
             Move to Github
           </a>
         </div>
